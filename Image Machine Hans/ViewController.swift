18b534b9a72597314ff5de700d0cae511eff5dac
Hans Christian Yulianto <hanschristianyulianto@gmail.com>
2020-03-05 17:52:58 +0700

    Initial Commit

---
+++ Image Machine Hans/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Machine Hans
+//
+//  Created by Hans Christian Yulianto on 05/03/20.
+//  Copyright © 2020 Hans Christian Yulianto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
